4d6e5b42e589b3ef65707135a4be70ae17421256
Morty <zaharzvagin254@gmail.com>
2026-09-14 00:28:55 +0300

    Apply mountain density directly to chunk generator settings

--- src/main/java/com/mortylovelly/skylimitless/mixin/RegistryLoaderMixin.java
+++ src/main/java/com/mortylovelly/skylimitless/mixin/RegistryLoaderMixin.java
@@ -1,5 +1,6 @@
 package com.mortylovelly.skylimitless.mixin;
 
+import com.mortylovelly.skylimitless.MountainHeightDensityFunction;
 import com.mortylovelly.skylimitless.SkyLimitlessConfig;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryLoader;
@@ -7,6 +8,8 @@ import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.DimensionTypes;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.GenerationShapeConfig;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import net.minecraft.world.gen.noise.NoiseRouter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -73,22 +76,52 @@ public abstract class RegistryLoaderMixin {
         int targetTopY = SkyLimitlessConfig.getEffectiveTopY();
         int targetHeight = targetTopY - originalShape.minimumY();
 
-        if (targetHeight <= originalShape.height()) {
-            return;
+        GenerationShapeConfig adjustedShape = originalShape;
+        if (targetHeight > originalShape.height()) {
+            adjustedShape = GenerationShapeConfig.create(
+                    originalShape.minimumY(),
+                    targetHeight,
+                    originalShape.horizontalSize(),
+                    originalShape.verticalSize()
+            );
         }
 
-        GenerationShapeConfig adjustedShape = GenerationShapeConfig.create(
-                originalShape.minimumY(),
-                targetHeight,
-                originalShape.horizontalSize(),
-                originalShape.verticalSize()
+        NoiseRouter originalRouter = originalSettings.noiseRouter();
+        DensityFunction adjustedFinalDensity = originalRouter.finalDensity();
+        int mountainHeight = SkyLimitlessConfig.getMountainHeight();
+
+        if (mountainHeight > SkyLimitlessConfig.VANILLA_TOP_Y) {
+            double scale = MountainHeightDensityFunction.getScaleForMountainHeight(mountainHeight);
+            adjustedFinalDensity = new MountainHeightDensityFunction(adjustedFinalDensity, scale);
+        }
+
+        NoiseRouter adjustedRouter = new NoiseRouter(
+                originalRouter.barrierNoise(),
+                originalRouter.fluidLevelFloodednessNoise(),
+                originalRouter.fluidLevelSpreadNoise(),
+                originalRouter.lavaNoise(),
+                originalRouter.temperature(),
+                originalRouter.vegetation(),
+                originalRouter.continents(),
+                originalRouter.erosion(),
+                originalRouter.depth(),
+                originalRouter.ridges(),
+                originalRouter.initialDensityWithoutJaggedness(),
+                adjustedFinalDensity,
+                originalRouter.veinToggle(),
+                originalRouter.veinRidged(),
+                originalRouter.veinGap()
         );
+
+        if (adjustedShape == originalShape && adjustedRouter == originalRouter) {
+            return;
+        }
 
         ChunkGeneratorSettings adjustedSettings = new ChunkGeneratorSettings(
                 adjustedShape,
                 originalSettings.defaultBlock(),
                 originalSettings.defaultFluid(),
-                originalSettings.noiseRouter(),
+                adjustedRouter,
                 originalSettings.surfaceRule(),
                 originalSettings.spawnTarget(),
                 originalSettings.seaLevel(),
